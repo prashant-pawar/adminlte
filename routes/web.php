f5e40b286efc7112c01aa1bd50cc8f7952ed01e3
Prashant <prashant456pawar@gmail.com>
2021-01-22 19:28:45 +0530

    change in web file

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('index');
+Route::get('/', function () {
+    return view('welcome');
 });
-
+/*
 Route::get('/form', function () {
     return view('blank');
 });
